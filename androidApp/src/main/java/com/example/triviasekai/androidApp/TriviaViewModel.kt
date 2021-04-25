6a674cd7b5840b642171903984e51fbe7cdf4b43
Zach Sao <zakaria.sao@gmail.com>
2021-04-25 17:50:22 -0400

    viewModel: decode html format

--- androidApp/src/main/java/com/example/triviasekai/androidApp/TriviaViewModel.kt
+++ androidApp/src/main/java/com/example/triviasekai/androidApp/TriviaViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.triviasekai.androidApp
 
+import android.text.Html
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -10,10 +11,11 @@ import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
+import java.net.URLDecoder
 
 class TriviaViewModel : ViewModel() {
 
-    private val categoriesSharedFlow = MutableSharedFlow<List<Category>>()
+    private val categoriesSharedFlow = MutableSharedFlow<List<Category>>(replay = 1)
     private val currentQuestionSharedFlow = MutableSharedFlow<Pair<TriviaResult, Int>>()
     private val triviaSDK = TriviaSDK()
 
@@ -35,7 +37,13 @@ class TriviaViewModel : ViewModel() {
         viewModelScope.launch {
             val results = triviaSDK.getQuestions(categoryId).results
             Log.d("zsao", "${results.size} results retrieved")
-            questions = results
+            questions = results.map {
+                it.copy(
+                    question = Html.fromHtml(it.question, Html.FROM_HTML_MODE_LEGACY).toString(),
+                    incorrectAnswers = it.incorrectAnswers.map { Html.fromHtml(it, Html.FROM_HTML_MODE_LEGACY).toString() },
+                    correctAnswer = Html.fromHtml(it.correctAnswer, Html.FROM_HTML_MODE_LEGACY).toString()
+                )
+            }
             currentQuestionSharedFlow.emit(Pair(results.first(), 0))
         }
     }
