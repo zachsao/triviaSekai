85ef2a211c8cca7dff5f1715ece490ff7bbbfc0e
Zach Sao <zakaria.sao@gmail.com>
2021-04-25 11:52:54 -0400

    bump gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript {
         val kotlinVersion = "1.4.31"
         val sqlDelightVersion: String by project
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        classpath("com.android.tools.build:gradle:7.0.0-alpha12")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha14")
         classpath("org.jetbrains.kotlin:kotlin-serialization:$kotlinVersion")
         classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
     }
