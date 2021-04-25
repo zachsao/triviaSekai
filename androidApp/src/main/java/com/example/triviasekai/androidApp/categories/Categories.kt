8c1650ce58deaceba1912f2f0cd6296e66947ba5
Zach Sao <zakaria.sao@gmail.com>
2021-04-25 14:31:35 -0400

    categories: show difficulty levels dialog

--- androidApp/src/main/java/com/example/triviasekai/androidApp/categories/Categories.kt
+++ androidApp/src/main/java/com/example/triviasekai/androidApp/categories/Categories.kt
@@ -1,21 +1,21 @@
 package com.example.triviasekai.androidApp.categories
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Divider
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.triviasekai.androidApp.TriviaViewModel
 import com.example.triviasekai.shared.model.Category
 
@@ -28,33 +28,31 @@ fun CategoriesScreen(viewModel: TriviaViewModel) {
 
 @Composable
 fun CategoriesContent(list: List<Category>) {
-    Column {
-        Text(
-            text = "Select a category",
-            Modifier
-                .fillMaxWidth(1f)
-                .padding(top = 16.dp),
-            textAlign = TextAlign.Center,
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp
-        )
-        CategoriesList(list = list)
+    val showDialog = remember { mutableStateOf(false) }
+    Scaffold(
+        topBar = { TopAppBar(title = { Text(text = "Select a category") }) }
+    ) {
+        Column {
+            CategoriesList(list = list) { showDialog.value = true }
+            LevelDialog(showDialog.value) { showDialog.value = false }
+        }
     }
+
 }
 
 @Composable
-fun CategoriesList(list: List<Category>) {
+fun CategoriesList(list: List<Category>, onItemClick: () -> Unit) {
     LazyColumn(
-        Modifier
-            .fillMaxHeight(1f)
-            .padding(16.dp)
+        Modifier.fillMaxHeight(1f),
+        contentPadding = PaddingValues(16.dp)
     ) {
         items(list) {
             Text(
                 text = it.name,
                 Modifier
                     .fillMaxWidth(1f)
-                    .padding(top = 8.dp, bottom = 8.dp),
+                    .padding(top = 8.dp, bottom = 8.dp)
+                    .clickable(onClick = onItemClick),
                 textAlign = TextAlign.Center
             )
             Divider()
